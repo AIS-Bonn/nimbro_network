09f7ebd9f91b7df07bd50fd6db783aa047df6d7b
Max Schwarz <max.schwarz@ais.uni-bonn.de>
2017-03-27 12:21:25 +0200

    nimbro_topic_transport: test: test all smaller msg sizes

--- nimbro_topic_transport/test/unit/test_packetizer.cpp
+++ nimbro_topic_transport/test/unit/test_packetizer.cpp
@@ -23,7 +23,7 @@ void testMessage(const Message::Ptr& msg, float fec)
 
 	auto packets = topicPacketizer->packetize(msg);
 
-	printf("FEC %f => Got %d packets\n", fec, (int)packets.size());
+// 	printf("FEC %f => Got %d packets\n", fec, (int)packets.size());
 
 	// Let's shuffle the packets
 	{
@@ -36,7 +36,7 @@ void testMessage(const Message::Ptr& msg, float fec)
 	{
 		std::size_t sourceSymbols = packets.size() / (1.0f + fec);
 		std::size_t drop_surplus = (fec / 2.0f) * sourceSymbols;
-		printf("Dropping %lu packets\n", drop_surplus);
+// 		printf("Dropping %lu packets\n", drop_surplus);
 		packets.resize(packets.size() - drop_surplus);
 	}
 
@@ -100,6 +100,23 @@ TEST_CASE("small", "[packetizer]")
 		testMessage(msg, 0.5);
 }
 
+TEST_CASE("increasing", "[packetizer]")
+{
+	for(std::size_t size = 1; size < 2048; ++size)
+	{
+		auto msg = std::make_shared<Message>();
+		msg->counter = 10;
+		msg->flags = Message::FLAG_COMPRESSED_ZSTD;
+		msg->type = "std_msgs/String";
+		msg->md5 = "992ce8a1687cec8c8bd883ec73ca41d1";
+		msg->payload.resize(size);
+		for(std::size_t i = 0; i < msg->payload.size(); ++i)
+			msg->payload[i] = i;
+
+		testMessage(msg, 0.5);
+	}
+}
+
 TEST_CASE("huge", "[packetizer]")
 {
 	auto msg = std::make_shared<Message>();
@@ -107,7 +124,7 @@ TEST_CASE("huge", "[packetizer]")
 	msg->flags = Message::FLAG_COMPRESSED_ZSTD;
 	msg->type = "std_msgs/String";
 	msg->md5 = "992ce8a1687cec8c8bd883ec73ca41d1";
-	msg->payload.resize(1024 * 1024);
+	msg->payload.resize(5 * 1024 * 1024);
 	for(std::size_t i = 0; i < msg->payload.size(); ++i)
 		msg->payload[i] = i;
 
