76016c1dd6cd69c6032d8b947a5fa59e4ade51a0
Max Schwarz <max.schwarz@ais.uni-bonn.de>
2021-11-05 16:17:08 +0100

    tf_throttle: tf_static_repeater: add publisher filter

--- nimbro_tf_throttle/src/tf_static_repeater.cpp
+++ nimbro_tf_throttle/src/tf_static_repeater.cpp
@@ -6,14 +6,59 @@
 #include <tf2_ros/buffer.h>
 #include <tf2_msgs/TFMessage.h>
 
+#include <unordered_set>
+
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "tf_static_repeater");
 
 	ros::NodeHandle nh{"~"};
 
+	std::unordered_set<std::string> excludePublishers;
+
+	{
+		using Value = XmlRpc::XmlRpcValue;
+
+		Value list;
+		if(nh.getParam("exclude_publishers", list))
+		{
+			if(list.getType() != Value::TypeArray)
+				throw std::runtime_error{"exclude_publishers should be a list"};
+
+			for(int i = 0; i < list.size(); ++i)
+			{
+				XmlRpc::XmlRpcValue entry = list[i];
+				if(entry.getType() != XmlRpc::XmlRpcValue::TypeString)
+					throw std::runtime_error{"exclude_publishers should be a list of strings"};
+
+				std::string s = entry;
+				excludePublishers.insert(ros::names::resolve(s));
+			}
+		}
+	}
+
 	tf2_ros::Buffer tfBuffer;
 	auto handleTransform = [&](const ros::MessageEvent<tf2_msgs::TFMessage>& event){
+		// Is this publisher allowed?
+		if(!excludePublishers.empty())
+		{
+			auto& connHeader = event.getConnectionHeader();
+			auto it = connHeader.find("callerid");
+
+			if(it == connHeader.end())
+				ROS_WARN_ONCE("No callerid in connection header");
+			else
+			{
+				if(excludePublishers.find(it->second) != excludePublishers.end())
+				{
+					ROS_DEBUG_NAMED("filter", "Caller ID: '%s' in exclusion list, not sending", it->second.c_str());
+					return;
+				}
+				else
+					ROS_DEBUG_NAMED("filter", "Caller ID: '%s' not in exclusion list, sending", it->second.c_str());
+			}
+		}
+
 		auto msg = event.getMessage();
 
 		for(auto& trans : msg->transforms)
