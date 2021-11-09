39a768db1bb969719e1cc293ffae087ae07e1487
Max Schwarz <max.schwarz@ais.uni-bonn.de>
2021-11-09 14:00:58 +0100

    topic_transport: tool to measure tf latency

--- nimbro_topic_transport/utils/measure_tf_latency.cpp
+++ nimbro_topic_transport/utils/measure_tf_latency.cpp
@@ -1,26 +1,30 @@
-// Receive msg from send_time and measure latency
+// Receive msg from tf and measure latency
 // Author: Max Schwarz <max.schwarz@ais.uni-bonn.de>
 
 #include <ros/ros.h>
 
-#include <std_msgs/Header.h>
+#include <tf2_msgs/TFMessage.h>
 
-void handleMsg(const ros::MessageEvent<std_msgs::Header>& msg)
+void handleMsg(const ros::MessageEvent<tf2_msgs::TFMessage>& msg)
 {
+	auto tfMsg = msg.getMessage();
+	if(tfMsg->transforms.empty())
+		return;
+
 	ros::Time now = ros::Time::now();
 	ROS_INFO("Latency: %8.2fms (of which %3.2fms is internal ROS latency)",
-		(now - msg.getMessage()->stamp).toSec() * 1000.0,
+		(now - tfMsg->transforms[0].header.stamp).toSec() * 1000.0,
 		(now - msg.getReceiptTime()).toSec() * 1000.0
 	);
 }
 
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "measure_latency", ros::init_options::AnonymousName);
+	ros::init(argc, argv, "measure_tf_latency", ros::init_options::AnonymousName);
 
 	if(argc != 2)
 	{
-		fprintf(stderr, "Usage: measure_latency [topic]\n");
+		fprintf(stderr, "Usage: measure_tf_latency [topic]\n");
 		return 1;
 	}
 
@@ -31,3 +35,4 @@ int main(int argc, char** argv)
 
 	return 0;
 }
+
